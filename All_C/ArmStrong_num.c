12ed7037a7c010f762a15b123620aa7b5c1134d2
Aashutosh Jha <64409843+aashutosh2102@users.noreply.github.com>
2023-08-05 14:21:27 +0530

    Changes as per Shystem

--- All_C/ArmStrong_num.c
+++ All_C/ArmStrong_num.c
@@ -1,49 +1,3 @@
-#include "armstrong_numbers.h"
-// #include<math.h>
-
-bool is_armstrong_number(int candidate){
-    int num,rem,ans=0,count=0,num2;
-    num=candidate;    
-    num2=candidate;
-    while(num>0)
-    {
-        num = num/10;
-        count++;
-    }
-    // count = countt(num);
-    while(num2>0)
-    {
-        rem = num2%10;
-        int val=1;
-        for(int i=0;i<count;i++)
-        {
-            val *= rem;
-        }
-        ans = ans + val;
-        num2 = num2/10;
-    }
-    if(ans==candidate)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-
-
-
-
-#ifndef ARMSTRONG_NUMBERS_H
-#define ARMSTRONG_NUMBERS_H
-#include <stdbool.h>
-bool is_armstrong_number(int candidate);
-#endif
-
-
-/*
 // My Code
 
 #include<stdio.h>
@@ -51,4 +5,3 @@ int main()
 {
     int i,cc=0,j;
 }
-*/
